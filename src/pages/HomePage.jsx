1a5b0b655789149ddfb6df873ac3b4ddb47e0731
macbook <harisgorcevic1000@gmail.com>
2024-07-24 16:17:02 +0200

    work

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,27 +17,11 @@ function HomePage() {
   const [isBlue, setIsBlue] = useState(null);
   const [position, setPosition] = useState({ top: 0, left: 0 });
   const [position2, setPosition2] = useState({ top: 0, left: 0 });
-  const { userID, setUserID } = useContext(Context);
+  const [checkRoom, setCheckRoom] = useState(0);
+  const [userID, setUserID] = useState(null);
 
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
-
-  function joinRoom() {
-    axios
-      .post("http://192.168.0.102:8000/api/send/room", {
-        userID: userID,
-        room: room,
-      })
-      .then((response) => {
-        console.log(response.data, "podaci");
-      })
-      .catch((error) => {
-        console.error("There was an error inserting the room!", error);
-      });
-
-    socket.emit("join_room", room);
-    console.log(room, "duzina sobe");
-  }
 
   useEffect(() => {
     axios
@@ -45,6 +29,9 @@ function HomePage() {
       .then((response) => {
         if (response.data.Status === "Success") {
           setIme(response.data.name);
+          setUserID(response.data.userID);
+          // setRoom(response.data?.room);
+          console.log(response.data, "svi datas");
         } else {
           console.log("greska");
         }
@@ -53,6 +40,41 @@ function HomePage() {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  function joinRoom() {
+    // axios
+    //   .post("http://192.168.0.102:8000/api/send/room", {
+    //     userID: userID,
+    //     room: room,
+    //   })
+    //   .then((response) => {
+    //     console.log(response.data, "podaci");
+    //   })
+    //   .catch((error) => {
+    //     console.error("There was an error inserting the room!", error);
+    //   });
+
+    socket.emit("join_room", room);
+    console.log(room, "duzina sobe");
+  }
+  console.log(userID, "id od usera");
+  // useEffect(() => {
+  //   function getData() {
+  //     axios
+  //       .get("http://192.168.0.102:8000/api/get/room", {
+  //         params: { userID: userID },
+  //       })
+  //       .then((res) => {
+  //         if (res.data?.room !== undefined) {
+  //           setRoom(res.data.room);
+  //         } else {
+  //           setRoom(res.data.room);
+  //         }
+  //         console.log(res.data?.room);
+  //       });
+  //   }
+  //   getData();
+  // }, [userID]);
 
   function sendMessage() {
     const currentTime = new Date().toLocaleTimeString();
@@ -179,6 +201,8 @@ function HomePage() {
       ></div>
       <div className="chat">
         <input
+          type="text"
+          // value={room}
           onChange={(e) => setRoom(e.target.value)}
           placeholder="Join Room"
         ></input>
